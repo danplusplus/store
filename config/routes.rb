d958d442adeb1ea695000cc09c6c3995e02a0c22
danplusplus <dluciano@djlsoftware.com>
2026-04-11 12:21:40 -0400

    Back in stock notifications

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+   resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+
   root "products#index"
   resources :products
 

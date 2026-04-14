1156562b7aa50f0a3c821b3b9dd18a5ec1d968a7
danplusplus <dluciano@djlsoftware.com>
2026-04-14 12:10:20 -0400

    Unsubscribe link

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
    resources :products do
     resources :subscribers, only: [ :create ]
   end
+  resource :unsubscribe, only: [ :show ]
 
   root "products#index"
   resources :products

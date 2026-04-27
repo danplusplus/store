8bc7db8973f3da4c6080c4f3abcd09528c003982
danplusplus <dluciano@djlsoftware.com>
2026-04-27 08:32:38 -0400

    After running rubocop

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,5 +7,4 @@ class Product < ApplicationRecord
 
   validates :name, presence: true
   validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
    resources :products do
     resources :subscribers, only: [ :create ]
